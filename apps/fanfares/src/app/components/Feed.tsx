0a0171dfaee89a998f0441d38a8269ed8280cdfa
WEB5D <info@blkluv.org>
2026-03-22 09:09:47 -0400

    Update Feed.tsx

--- apps/fanfares/src/app/components/Feed.tsx
+++ apps/fanfares/src/app/components/Feed.tsx
@@ -1,45 +1,41 @@
 "use client"
-import { generatePrivateKey } from "nostr-tools"
-import { useAppState } from "../controllers/state/old/use-app-state"
+
+import { usePrimalNotes, usePrimalProfiles, usePrimalNoteStats, usePrimalIsFetching } from "../controllers/state/primal-slice"
 import { FeedPost } from "./FeedPost"
-import {
-  usePrimalNotes,
-  usePrimalProfiles,
-  usePrimalNoteStats,
-  usePrimalIsFetching,
-} from "../controllers/state/primal-slice"
 import Lottie from "lottie-react"
 import { LottieLoading } from "./Lottie"
 
 export function Feed() {
-  const primalNotes = usePrimalNotes()
-  const primalProfiles = usePrimalProfiles()
-  const primalNoteStats = usePrimalNoteStats()
+  const primalNotes = usePrimalNotes() || {}
+  const primalProfiles = usePrimalProfiles() || {}
+  const primalNoteStats = usePrimalNoteStats() || {}
   const primalIsFetching = usePrimalIsFetching()
-
-  //TODO fix renders
-  // console.log("Render Feed -- " + primalNotes.length);
 
   const renderLoading = () => {
     return (
-      <div className="w-full h-full flex items-center">
-        <Lottie className="w-20" animationData={LottieLoading} loop={true} />
-        <p className="">Loading...</p>
+      <div className="w-full h-full flex items-center gap-2">
+        <Lottie className="w-20" animationData={LottieLoading} loop />
+        <p>Loading...</p>
       </div>
     )
   }
 
   const renderNotes = () => {
-    return Object.values(primalNotes).map(note => {
+    return Object.values(primalNotes).map((note: any) => {
+      if (!note) return null
+
       const profile = primalProfiles[note.pubkey]
       const stats = primalNoteStats[note.id]
 
-      if (!profile) {
-        return null
-      }
+      if (!profile) return null
 
       return (
-        <FeedPost key={note.id} note={note} profile={profile} stats={stats} />
+        <FeedPost
+          key={note.id}
+          note={note}
+          profile={profile}
+          stats={stats}
+        />
       )
     })
   }
@@ -47,8 +43,9 @@ export function Feed() {
   return (
     <>
       <h1 className="font-black text-center text-gray-100 text-xl/4 md:mt-4 md:text-start md:text-4xl">
-        Nostr Universe{" "}
+        Nostr Universe
       </h1>
+
       {primalIsFetching ? renderLoading() : renderNotes()}
     </>
   )
